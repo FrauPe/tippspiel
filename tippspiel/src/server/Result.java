b55652e9ba0f60e6fba42ec8c4c2d889750d79fd
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-12-13 08:37:51 +0100

    Add missing class/file

---
+++ tippspiel/src/server/Result.java
@@ -0,0 +1,12 @@
+package server;
+
+public final class Result {
+
+	public final int goals1, goals2;
+
+	public Result(int goals1, int goals2) {
+		this.goals1 = goals1;
+		this.goals2 = goals2;
+	}
+
+}
